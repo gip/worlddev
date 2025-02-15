7e0cf9a5dfafffc48bf4940e38842ff86e9a012e
Gilles Pirio <g36130@gmail.com>
2025-02-15 10:15:47 -0800

    fix: logout button

--- next-mini-app/app/page.tsx
+++ next-mini-app/app/page.tsx
@@ -21,6 +21,14 @@ export default function Home() {
             <div>You are logged in!</div>
             <div>Welcome {session?.user?.username}!</div>
             <div>Your wallet address is: {session?.user?.walletAddress}</div>
+            <div className="flex gap-4 items-center flex-col sm:flex-row">
+              <button
+                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+                onClick={signOut}
+              >
+                Logout
+              </button>
+            </div>
           </>
         ) : (
           <>
@@ -37,12 +45,6 @@ export default function Home() {
                 onClick={signIn}
               >
                 Login with World Wallet
-              </button>
-              <button
-                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                onClick={signOut}
-              >
-                Logout
               </button>
             </div>
           </>
