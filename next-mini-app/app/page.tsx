34c8ce576237db84a9d503f306e9fe872b4d448b
Gilles Pirio <g36130@gmail.com>
2025-04-19 21:06:54 -0700

    feat: support web context

--- next-mini-app/app/page.tsx
+++ next-mini-app/app/page.tsx
@@ -14,10 +14,32 @@ export default function Home() {
         {!isInstalled && <>
           <div>This app is designed to be run on <a href="https://worldcoin.org/mini-app?app_id=app_a963cd2077f59caf1146198685eed59a&draft_id=meta_4d75d4955b27044f4ef562e60ad09d17" target="_blank" rel="noopener noreferrer">World App</a></div>
           <Image src="/miniappqr.png" alt="Mini App QR Code" width={400} height={400} />
+          <hr style={{ width: "100px", margin: 10 }} />
+          {session && session.isAuthenticatedWorldID && <>
+            <div>You have authenticated with <span className="underline">World ID</span></div>
+            <div>Your unique app ID is available</div>
+            <div>Orb Verification Status: <span className="font-bold">{session.isOrbVerified ? 'verified ✓' : 'not verified ✗'}</span></div>
+            <button
+                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+                onClick={signOut}
+              >
+                Logout
+              </button>
+          </>}
+          {!(session && session.isAuthenticatedWorldID) && <>
+            <div>In a web context, it is however possible to sign in with World ID</div>
+            <div>You will need the <a href="https://worldcoin.org/world-app" target="_blank" rel="noopener noreferrer">World App</a> to sign in with World ID</div>
+            <button
+              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+              onClick={() => signInWorldID(null)}
+            >
+              Sign in with World ID
+            </button>
+          </>}
         </>}
         {isInstalled && (isAuthenticated && session ? (
           <>
-          <hr style={{ width: "100px", margin: 10 }} />
+            <hr style={{ width: "100px", margin: 10 }} />
             <div>You are logged in!</div>
             <div className="flex gap-1 items-center flex-col sm:flex-row">
               <button
